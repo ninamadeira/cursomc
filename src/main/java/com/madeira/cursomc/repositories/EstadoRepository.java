5d76fe7050021b472c3483e889d8c336815a04e2
ninamadeira <nina.madeira@gmail.com>
2020-11-24 21:32:05 -0300

    Estado e cidade

---
+++ src/main/java/com/madeira/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,10 @@
+package com.madeira.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.madeira.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}
