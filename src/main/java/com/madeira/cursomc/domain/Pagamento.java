de050ed8441bdcf4e767d14c17e3107ff80a4aed
ninamadeira <nina.madeira@gmail.com>
2020-11-26 17:41:14 -0300

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/madeira/cursomc/domain/Pagamento.java
+++ src/main/java/com/madeira/cursomc/domain/Pagamento.java
@@ -10,16 +10,19 @@ import javax.persistence.JoinColumn;
 import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.madeira.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
 public abstract class Pagamento implements Serializable {
+	private static final long serialVersionUID = 1L;
 
 	@Id
 	private Integer id;
 	private Integer estado;
 
+	@JsonBackReference
 	@OneToOne
 	@JoinColumn(name = "pedido_id")
 	@MapsId
