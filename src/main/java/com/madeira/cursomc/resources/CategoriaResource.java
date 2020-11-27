403530126c157f3a91139ddd1758a396cd5f9536
ninamadeira <nina.madeira@gmail.com>
2020-11-27 19:44:18 -0300

    Atualizando Categoria com PUT, outros pequenos ajustes

--- src/main/java/com/madeira/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/madeira/cursomc/resources/CategoriaResource.java
@@ -1,14 +1,13 @@
 package com.madeira.cursomc.resources;
 
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,34 +19,30 @@ import com.madeira.cursomc.services.CategoriaService;
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	
+
 	@Autowired
 	private CategoriaService service;
-	
-	
-	@GetMapping
-	public List<Categoria> listar() {
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(new Categoria(1, "Informática"));
-		lista.add(new Categoria(2, "Escritório"));
 
-		return lista;
-	}
-	
 	@GetMapping("/{id}")
-	public ResponseEntity<?> find(@PathVariable Integer id) {
+	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
 		Categoria obj = service.find(id);
-		System.out.println(obj);
 		return ResponseEntity.ok().body(obj);
-		
 	}
 	
 	@PostMapping
 	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
-			.path("/{id}").buildAndExpand(obj.getId()).toUri();
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id) {
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+
 
 }
