b3fb79929b67f001f18fa7d63c7f2a2809edbdbb
ninamadeira <nina.madeira@gmail.com>
2020-11-29 16:17:47 -0300

    Validacao Bean Validation

--- src/main/java/com/madeira/cursomc/services/CategoriaService.java
+++ src/main/java/com/madeira/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.madeira.cursomc.domain.Categoria;
+import com.madeira.cursomc.dto.CategoriaDTO;
 import com.madeira.cursomc.repositories.CategoriaRepository;
 import com.madeira.cursomc.services.exception.DataIntegrityException;
 import com.madeira.cursomc.services.exception.ObjectNotFoundException;
@@ -54,5 +55,9 @@ public class CategoriaService {
 		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
 		}
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
