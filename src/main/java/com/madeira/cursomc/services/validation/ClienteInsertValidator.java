a52b9681792bba1320d0bc261ec245c3e3419597
ninamadeira <nina.madeira@gmail.com>
2020-11-30 19:59:01 -0300

    Validacao customizada: email nao repetido na insercao de Cliente

--- src/main/java/com/madeira/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/madeira/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.madeira.cursomc.domain.Cliente;
 import com.madeira.cursomc.domain.enums.TipoCliente;
 import com.madeira.cursomc.dto.ClienteNewDTO;
+import com.madeira.cursomc.repositories.ClienteRepository;
 import com.madeira.cursomc.resources.exception.FieldMessage;
 import com.madeira.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
 
 	@Override
 	public void initialize(ClienteInsert ann) {
@@ -21,6 +28,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
 
 		List<FieldMessage> list = new ArrayList<>();
+
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
 
 		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
