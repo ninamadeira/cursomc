e57fcd41b22e9635120a8b1b9495ffb17198289c
ninamadeira <nina.madeira@gmail.com>
2020-12-30 19:55:39 -0300

    Enviando imagem via endpoint

--- src/main/java/com/madeira/cursomc/services/ClienteService.java
+++ src/main/java/com/madeira/cursomc/services/ClienteService.java
@@ -1,5 +1,6 @@
 package com.madeira.cursomc.services;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,6 +12,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.madeira.cursomc.domain.Cidade;
 import com.madeira.cursomc.domain.Cliente;
@@ -38,12 +40,15 @@ public class ClienteService {
 	@Autowired
 	private BCryptPasswordEncoder pe;
 
+	@Autowired
+	private S3Service s3Service;
+
 	public Cliente find(Integer id) {
 		UserSS user = UserService.authenticated();
-		if (user==null || !user.hasRole(Perfil.ADMIN) && !id.equals(user.getId())) {
+		if (user == null || !user.hasRole(Perfil.ADMIN) && !id.equals(user.getId())) {
 			throw new AuthorizationException("Acesso negado");
 		}
-		
+
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
@@ -100,6 +105,10 @@ public class ClienteService {
 			cli.getTelefones().add(objDto.getTelefone3());
 		}
 		return cli;
+	}
+
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		return s3Service.uploadFile(multipartFile);
 	}
 
 	private void updateData(Cliente newObj, Cliente obj) {
